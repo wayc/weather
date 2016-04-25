f2e54286fd017addeb2fb8e37ce4316f42e10df9
Way C <dimsumboba@gmail.com>
2016-04-24 22:09:06 -0700

    Fix VERBOSE case

--- script/wxug_loop.sh
+++ script/wxug_loop.sh
@@ -3,15 +3,14 @@
 PATH_TO_SEND_WEATHER=$(pwd)/wxug_send_weather.sh
 VERBOSE=0
 
-case "$1" in
--v|--v|--ve|--ver|--verb|--verbo|--verbos|--verbose)
+case "$1" in -v|--v|--ve|--ver|--verb|--verbo|--verbos|--verbose)
     VERBOSE=1
     shift ;;
 esac
 
 while true
 do
-	if [ "$verbose" = 1 ]; then
+	if [ "$VERBOSE" = 1 ]; then
 		bash $PATH_TO_SEND_WEATHER
 	else
 		bash $PATH_TO_SEND_WEATHER >> /dev/null 2>&1

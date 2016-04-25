418b7640e81a519921354bc84cead08dbc82fdf1
Way C <dimsumboba@gmail.com>
2016-04-24 22:05:13 -0700

    Add Verbose option to loop script

--- script/wxug_loop.sh
+++ script/wxug_loop.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
 PATH_TO_SEND_WEATHER=$(pwd)/wxug_send_weather.sh
+VERBOSE=0
+
+case "$1" in
+-v|--v|--ve|--ver|--verb|--verbo|--verbos|--verbose)
+    VERBOSE=1
+    shift ;;
+esac
 
 while true
 do
-	bash $PATH_TO_SEND_WEATHER >> /dev/null 2>&1
+	if [ "$verbose" = 1 ]; then
+		bash $PATH_TO_SEND_WEATHER
+	else
+		bash $PATH_TO_SEND_WEATHER >> /dev/null 2>&1
+	fi
 	sleep 1
 done
